e3afaa7ccd4897a81f000b2f62609fde9682b2d7
AjeebKt <ajeebajb@gmail.com>
2015-07-05 16:24:24 +0530

    login page completed

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 	if (isset($_SESSION['username']) and isset($_SESSION['password'])) 
 	{
-		header("location:Dashbord.php");
+		header("location:Dashboard.php");
 	}
  ?>
 
@@ -11,21 +11,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="./css/normalize.css">
-	<link rel="stylesheet" href="./css/css.css">
-	<link rel="stylesheet" href="./css/style1.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/css.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class="bg-login">
-		<form id="loginForm" class="login" method="POST">
+	<div class="login">
+		<form id="loginForm" method="POST">
 			<h3>Login</h3>
-			<div>
-				<a class="user"></a>
-				<input id="text" placeholder="User Name" type="text">
-			</div>
-			<div>
-				<a class="lock"></a>
-				<input id="Password" placeholder="Password" type="password">
+			<div class="container">
+				<div>
+					<a class="user"></a>
+					<input id="text" placeholder="User Name" type="text">
+				</div>
+				<div>
+					<a class="lock"></a>
+					<input id="Password" placeholder="Password" type="password">
+				</div>
 			</div>
 			<button id="loginButton">Login</button>
 		</form>
