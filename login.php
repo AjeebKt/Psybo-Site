c576ae60edafc22a56a2cbd93cf128dba351c06d
AjeebKt <ajeebajb@gmail.com>
2015-07-06 10:26:04 +0530

    login page confict fixed

--- login.php
+++ login.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 <?php 
 	require_once("Database.php");
 	$objdb=new Database("localhost","root","asd","psybo-db");
@@ -31,15 +29,6 @@
  ?>
 
 <!DOCTYPE html>
-=======
-<?php 
-
-	if (isset($_SESSION['username']) and isset($_SESSION['password'])) 
-	{
-		header("location:Dashboard.php");
-	}
- ?>
->>>>>>> e844aa8d905fdffbe32bc912f6c90577731b7275
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -48,22 +37,9 @@
 	<link rel="stylesheet" href="css/css.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<<<<<<< HEAD
-<body>
-	<div class="bg-login">
-		<form id="loginForm" class="login" method="POST" action="">
-			<h3>Login</h3>
-			<div>
-				<a class="user"></a>
-				<input id="username" name="username" placeholder="User Name" type="text">
-			</div>
-			<div>
-				<a class="lock"></a>
-				<input id="password" name="password" placeholder="Password" type="password">
-=======
 <body style="background-color: #ccc;">
-	<div class="login">
-		<form id="loginForm" method="POST">
+	<form id="loginForm" method="POST">
+		<div class="login">
 			<h3>Login</h3>
 			<div class="container">
 				<div>
@@ -74,10 +50,9 @@
 					<a class="lock"></a>
 					<input id="Password" placeholder="Password" type="password">
 				</div>
->>>>>>> e844aa8d905fdffbe32bc912f6c90577731b7275
 			</div>
 			<button name="loginButton">Login</button>
-		</form>
-	</div>
+		</div>
+	</form>
 </body>
 </html>
