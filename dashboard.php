0871438bcafca358c822dcca3da4397c0a479ed7
AjeebKt <ajeebajb@gmail.com>
2015-07-06 09:47:50 +0530

    login page completed and Dashboard starts

--- dashboard.php
+++ dashboard.php
@@ -3,9 +3,29 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Dashboard</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/css.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<header>
+		
+	</header>
 	<aside>
+		<a href="#tabPortfolio"><button class="button1">tab 1</button></a>
+		<a href="#tabTeam"><button class="button2">tab 2</button></a>
 	</aside>
+	<section>
+		<div id="tabPortfolio" class="tab-portfolio">
+			<div>Insert</div>
+			<div>Update</div>
+			<div>Delete</div>
+		</div>
+		<div id="tabTeam" class="tab-team">
+			<div>Insert</div>
+			<div>Update</div>
+			<div>Delete</div>
+		</div>
+	</section>
 </body>
 </html>
--- login.php
+++ login.php
@@ -15,7 +15,7 @@
 	<link rel="stylesheet" href="css/css.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body style="background-color: #ccc;">
 	<div class="login">
 		<form id="loginForm" method="POST">
 			<h3>Login</h3>
