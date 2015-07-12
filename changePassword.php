c2e8b3a7e41322aa102254b05c89b99afa3d5fa0
AjeebKt <ajeebajb@gmail.com>
2015-07-12 23:17:28 +0530

    add changePassword.php page

---
+++ changePassword.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/theme.css">
+	<title>Change Password</title>
+</head>
+<body style="background-color:#008060;">
+	<div class="change-pwrd">
+		<h3>Change Password</h3>
+		<form action="" method="POST">
+			<label for="currentPwrd">Current Password</label>
+			<input id="currentPwrd" type="password">
+			<label for="newPwrd">New Password</label>
+			<input id="newPwrd" type="password">
+			<label for="confirmPwrd">Confirm Password</label>
+			<input id="confirmPwrd" type="password">
+			<button>Submit</button>
+			<button>Reset</button>
+		</form>
+	</div>
+</body>
+</html>
