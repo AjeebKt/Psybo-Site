6ff5ccbc2907c568bb4da73ae4e3c5c2dc0a599b
AjeebKt <ajeebajb@gmail.com>
2015-07-20 11:53:04 +0530

    modify footer

--- footer.php
+++ footer.php
@@ -1,23 +1,22 @@
 <footer>
-	<div class="container">
-		<div class="footer-details">
-			<ul class="social-links">
-				<li>
-					<a class="facebook" href="https://www.facebook.com/psybotechnologies"></a>
-				</li>
-				<li>
-					<a class="twitter" href="https://twitter.com/psybotech"></a>
-				</li>
-				<li>
-					<a class="linkedin" href="https://www.linkedin.php"></a>
-				</li>
-				<li>
-					<a class="gplus" href="https://plus.google.com/u/0/"></a>
-				</li>
-			</ul>	
-			<div class="site-details">
-				<p>All Right Recieved @ PSYBO Technologies PVT.LTD</p>
-				<p>PSYBO Technologies <?php echo(date("Y")); ?></p>
-			</div>
+	<div class="footer-details">
+		<ul class="social-links">
+			<li>
+				<a class="facebook" href="https://www.facebook.com/psybotechnologies"></a>
+			</li>
+			<li>
+				<a class="twitter" href="https://twitter.com/psybotech"></a>
+			</li>
+			<li>
+				<a class="linkedin" href="https://www.linkedin.php"></a>
+			</li>
+			<li>
+				<a class="gplus" href="https://plus.google.com/u/0/"></a>
+			</li>
+		</ul>	
+		<div class="site-details">
+			<p>All Right Recieved @ PSYBO Technologies PVT.LTD</p>
+			<p>PSYBO Technologies <?php echo(date("Y")); ?></p>
+		</div>
 	</div>
 </footer>
