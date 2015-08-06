ca9a024101852f5ed9f7be8a74e743e90f7bc7c7
Noushid <pnoushid@gmail.com>
2015-08-06 17:30:11 +0530

    no changed

---
+++ testnew.php
@@ -0,0 +1,77 @@
+
+ <!DOCTYPE html>
+<html>
+<body>
+
+<form action="" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+<!-- </form>
+<form action="my_upload.php" enctype="multipart/form-data" method="post">Photo <input name="image" size="30" type="file" /> <input name="upload" type="submit" value="Upload" /></form>
+
+
+ <form action="" method="post" enctype="multipart/form-data"> -->
+	<!-- <img <?php //echo "\"src=".$_FILES['fileToUpload']['tmp_name']."\""; ?> > -->
+<!-- </form> -->
+</body>
+</html> 
+
+
+<?php 
+
+function reduce_size($img , $source , $dest , $maxw , $maxh)
+{
+	$jpg = $source.$img;
+	if ($jpg) 
+	{
+		var_dump(getimagesize($jpg));
+		list($width , $height ) = getimagesize($jpg);//$type will return the type of the image
+		// var_dump($widkjoth);
+		$source = imagecreatefromjpeg($jpg);
+		if ( $maxw >= $width and $mxh >= $height) 
+		{
+			$ratio = 1;
+		} 
+		else if ($width > $height)
+		{
+			$ratio = $maxw / $maxh;
+
+		}
+		else
+		{
+			$ratio =$maxh / $maxw;
+
+		}
+
+		$thumb_width =round($width * $ratio);
+		$thumb_height = round($height * $ratio);
+
+		$thumb = imagecreatetruecolor($thumb_width, $thumb_height );
+		imagecopyresampled($thumb, $source, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+
+		$path = $dest.$img."_thumb.image";
+		imagejpeg($thumb , $path ,50);
+	}
+	imagedestroy($thumb);
+	imagedestroy($source);
+}
+if (isset($_POST['submit'])) 
+{
+	// $check1 = getimagesize($img);
+	// var_dump($check1);
+	if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], getcwd().'/test/'.basename($_FILES["fileToUpload"]["name"])) ) 
+	{
+		// var_dump($img);
+		$source = getcwd().'/test/';
+		$dest = getcwd().'/test/' ;
+		$img = basename($_FILES['fileToUpload']['name'] );
+		var_dump($img)	;
+		reduce_size($img , $source , $dest , 200 ,200 );
+		echo "success";
+	}
+}
+ ?>
+
+
+
--- tests.php
+++ tests.php
@@ -319,64 +319,92 @@ $str=".lorumips omis .dummi.text.it mostu sage for webdevelpors ";
 // echo 'hollow world<br>';
 
 // echo 'test';
+/////////////////////////////compress image///////////////////////////
+// $name = ''; $type = ''; $size = ''; $error = ''; 
+// function compress_image($source_url, $destination_url, $quality) 
+// { 
+// 	$info = getimagesize($source_url);
 
-$name = ''; $type = ''; $size = ''; $error = ''; 
-function compress_image($source_url, $destination_url, $quality) 
-{ 
-	$info = getimagesize($source_url);
+// 	if ($info['mime'] == 'image/jpeg') 
+// 		$image = imagecreatefromjpeg($source_url);
+// 	elseif ($info['mime'] == 'image/gif')
+// 		$image = imagecreatefromgif($source_url);
+// 	elseif ($info['mime'] == 'image/png')
+// 		$image = imagecreatefrompng($source_url);
 
-	if ($info['mime'] == 'image/jpeg') 
-		$image = imagecreatefromjpeg($source_url);
-	elseif ($info['mime'] == 'image/gif')
-		$image = imagecreatefromgif($source_url);
-	elseif ($info['mime'] == 'image/png')
-		$image = imagecreatefrompng($source_url);
+// 	// imagejpeg($image, $destination_url, $quality);
+// 	return $destination_url;
 
-	// imagejpeg($image, $destination_url, $quality);
-	return $destination_url;
+// } 
+// 	if ($_POST) 
+// 	{ 
+// 		if ($_FILES["file"]["error"] > 0) 
+// 		{
+// 			 $error = $_FILES["file"]["error"];
+// 		} 
+// 	else if (($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/pjpeg")) 
+// 	{ 
+// 		$url = 'destination .jpg';
+// 		$filename = compress_image($_FILES["file"]["tmp_name"], $url, 80);
+// 		$buffer = file_get_contents($url);
+// 		echo $buffer;
+// 		echo $filename;
+// 		if(!move_uploaded_file($filename, getcwd().'/upload-image'))
+// 		{
+// 			echo "error";
+// 			print_r($_FILES);
+// 		}
 
-} 
-	if ($_POST) 
-	{ 
-		if ($_FILES["file"]["error"] > 0) 
-		{
-			 $error = $_FILES["file"]["error"];
-		} 
-	else if (($_FILES["file"]["type"] == "image/gif") || ($_FILES["file"]["type"] == "image/jpeg") || ($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/pjpeg")) 
-	{ 
-		$url = 'destination .jpg';
-		$filename = compress_image($_FILES["file"]["tmp_name"], $url, 80);
-		$buffer = file_get_contents($url);
-		echo $buffer;
-		echo $filename;
-		if(!move_uploaded_file($filename, getcwd().'/upload-image'))
-		{
-			echo "error";
-			print_r($_FILES);
-		}
-		/* Force download dialog... */ 
-		// header("Content-Type: application/force-download");
-		// header("Content-Type: application/octet-stream");
-		// header("Content-Type: application/download");
+// 		/* Force download dialog... */ 
+// 		// header("Content-Type: application/force-download");
+// 		// header("Content-Type: application/octet-stream");
+// 		// header("Content-Type: application/download");
 
-		/* Don't allow caching... */ 
-		// header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+// 		/* Don't allow caching... */ 
+// 		// header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 
-		/* Set data type, size and filename */ 
-		// header("Content-Type: application/octet-stream");
-		// header("Content-Transfer-Encoding: binary");
-		// header("Content-Length: " . strlen($buffer));
-		// header("Content-Disposition: attachment;
-		// filename=$url");
+// 		/* Set data type, size and filename */ 
+// 		// header("Content-Type: application/octet-stream");
+// 		// header("Content-Transfer-Encoding: binary");
+// 		// header("Content-Length: " . strlen($buffer));
+// 		// header("Content-Disposition: attachment;
+// 		// filename=$url");
 
-		/* Send our file... */ 
-		echo $buffer;
-	}
-	else 
-	{
-		$error = "Uploaded image should be jpg or gif or png";
-	} 
-} 
+// 		/* Send our file... */ 
+// 		echo $buffer;
+// 	}
+// 	else 
+// 	{
+// 		$error = "Uploaded image should be jpg or gif or png";
+// 	} 
+// } 
+
+//////////////////////////////////
+
+// $info = array('coffee',  'caffeine');
+
+// // Listing all the variables
+// list($drink, $color, $power) = $info;
+// echo "$drink is $color and $power makes it special.\n";
+// var_dump($info);
+// var_dump($power);
+// Listing some of them
+// list($drink, , $power) = $info;
+// echo "$drink has $power.\n";
+
+// // Or let's skip to only the third one
+// list( , , $power) = $info;
+// echo "I need $power!\n";
+
+// // list() doesn't work with strings
+// list($bar) = "abcde";
+// var_dump($bar); // is_null(var)
+
+$check=getimagesize(getcwd().'/test/watermark_Your_Natural_Beauty_wallpaper.jpg');
+var_dump($check);
+list($aaa) = $check;
+var_dump($aaa);
+
 ?> 
 	<html> 
 	<head> 
