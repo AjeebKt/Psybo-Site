32565f7cbf5a4ba153b126184d0cf07228327831
AjeebKt <ajeebajb@gmail.com>
2015-08-07 19:17:36 +0530

    add our-vision div

--- about.php
+++ about.php
@@ -28,9 +28,9 @@
 	<section>
 		<div class="container">
 			<h1>About Us</h1>
-			<div class="show-tab">
+			<div class="about-us">
 				<div class="left">
-					<p>
+					<p class="p-bold">
 						Psybo technologies is a technology wise creative company doing extensive projects in the field of Website Designing, Website Development and Software Development.
 					</p>
 				</div>
@@ -38,6 +38,26 @@
 					<p>
 						Our goal is simple, to create excellent websites that make your business shine. Our team has been delivering innovative, cost effective and timely solutions that drive the growth of small and medium-sized businesses worldwide.
 					</p>
+				</div>
+			</div>
+			<div class="our-vision">
+				<div class="left lf-padding">
+					<h3>Our Vision</h3>
+					<p>Our team has been delivering innovative, cost effective and timely solutions that drive the growth of small and medium-sized businesses worldwide. Engineered by high quality professionals and managed by matured processes, we help forward-thinking companies achieve and surpass their business goals.</p>
+				</div>
+				<div class="right l-padding cut-l-pad">
+					<h3>Our Mission</h3>
+					<p>With mission to develop the clients performance in their business, our company is sincerely working harder for showing profit oriented results to their clients all over the World.“Together we build lasting relationships with the power of technology and our passion for extreme quality“.</p>
+				</div>
+				<div class="left lf-padding">
+					<h3>Our Quality Policy</h3>
+					<p>We treasure our client value and satisfaction. We strive for excellence and we are committed to deliver enhanced value to our customers with commitment and credibility.Psybo technologies is committed to continually improve the effectiveness of quality through team work, better tools and better technology.</p>
+				</div>
+				<div class="right l-padding cut-l-pad">
+					<h3>Our Methodology</h3>
+					<p><b>Customer is First :</b>Total commitment to our customers. To ensure that we achieve their business objectives and surpass their quality and service expectations fulfilled.<br><b>Personality:</b>
+We think that great design is a mix of beautiful visuals, an engaging story and an authentic personality.<br><b>Relationships:</b>
+Design and code only makes up one part of a great company. The rest is down to the close relationships we form with our clients,</p>
 				</div>
 			</div>
 		</div>
